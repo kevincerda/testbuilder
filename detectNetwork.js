c2c654a909145c326db6e082006e98a7cceec188
Kevin <kevin@cerda.io>
2018-03-31 23:13:00 -0700

    refactored. created two helperhelp functions to help refactor.

--- detectNetwork.js
+++ detectNetwork.js
@@ -2,52 +2,70 @@ const getPrefix = function(cardNumber, prefixLength) {
 	return parseInt(cardNumber.slice(0, prefixLength));
 };
 
+// containsPrefix will compare two arrays of prefixes and check if theres a matching value;
+// useful when a network has multiple prefix lengths;
+const containsPrefix = function(prefixes, prefix) {
+	return prefixes.some(el => prefix.includes(el));
+};
+
+const withinRange = function(prefix, min, max) {
+	return prefix >= min && prefix <= max;
+};
+
 const checkDinersClub = function(cardNumber) {
-	const dcLength = [14];
-	const dcPrefix = [38, 39];
-	return dcLength.includes(cardNumber.length) && dcPrefix.includes(getPrefix(cardNumber, 2));
+	let prefix = getPrefix(cardNumber, 2);
+	const lengths = [14];
+	const prefixes = [38, 39];
+	return lengths.includes(cardNumber.length) && prefixes.includes(prefix);
 };
 
 const checkAmericanExpress = function(cardNumber) {
-	const amLength = [15];
-	const amPrefix = [34, 37];
-	return amLength.includes(cardNumber.length) && amPrefix.includes(getPrefix(cardNumber, 2));
+	let prefix = getPrefix(cardNumber, 2);
+	const lengths = [15];
+	const prefixes = [34, 37];
+	return lengths.includes(cardNumber.length) && prefixes.includes(prefix);
 };
 
 const checkVisa = function(cardNumber) {
-	const vLength = [13, 16, 19];
-	const vPrefix = [4];
-	return vLength.includes(cardNumber.length) && vPrefix.includes(getPrefix(cardNumber, 1));
+	let prefix = getPrefix(cardNumber, 1);
+	const lengths = [13, 16, 19];
+	const prefixes = [4];
+	return lengths.includes(cardNumber.length) && prefixes.includes(prefix);
 };
 
 const checkMasterCard = function(cardNumber) {
-	const mcLength = [16];
-	const mPrefix = [51, 52, 53, 54, 55];
-	return mcLength.includes(cardNumber.length) && mPrefix.includes(getPrefix(cardNumber, 2));
+	let prefix = getPrefix(cardNumber, 2);
+	const lengths = [16];
+	const prefixes = [51, 52, 53, 54, 55];
+	return lengths.includes(cardNumber.length) && prefixes.includes(prefix);
 };
 
 const checkDiscover = function(cardNumber) {
-	const dLength = [16, 19];
-	const dPrefix = [65, 644, 645, 646, 647, 648, 649, 6011];
-	return dLength.includes(cardNumber.length) && (dPrefix.includes(getPrefix(cardNumber, 2)) || dPrefix.includes(getPrefix(cardNumber, 3)) || dPrefix.includes(getPrefix(cardNumber, 4)));
+	let prefix = [getPrefix(cardNumber, 2), getPrefix(cardNumber, 3), getPrefix(cardNumber, 4)];
+	const lengths = [16, 19];
+	const prefixes = [65, 644, 645, 646, 647, 648, 649, 6011];
+	return lengths.includes(cardNumber.length) && containsPrefix(prefixes, prefix);
 };
 
 const checkMaestro = function(cardNumber) {
-	const mLength = [12, 13, 14, 15, 16, 17, 18, 19];
-	const mPrefix = [5018, 5020, 5038, 6304];
-	return mLength.includes(cardNumber.length) && (mPrefix.includes(getPrefix(cardNumber,4)));
+	let prefix = getPrefix(cardNumber, 4);
+	const lengths = [12, 13, 14, 15, 16, 17, 18, 19];
+	const prefixes = [5018, 5020, 5038, 6304];
+	return lengths.includes(cardNumber.length) && prefixes.includes(prefix);
 };
 
 const checkChinaUnionPay = function(cardNumber) {
-	const cupLength = [16, 17, 18, 19];
-	const cupPrefix = [624, 625, 626, 6282, 6283, 6284, 6285, 6286, 6287, 6288];
-	return cupLength.includes(cardNumber.length) && ((cupPrefix.includes(getPrefix(cardNumber, 3)) || cupPrefix.includes(getPrefix(cardNumber, 4)) || (getPrefix(cardNumber, 6) >= 622126 && getPrefix(cardNumber, 6) <= 622925)));
+	let prefix = [getPrefix(cardNumber, 3), getPrefix(cardNumber, 4)];
+	const lengths = [16, 17, 18, 19];
+	const prefixes = [624, 625, 626, 6282, 6283, 6284, 6285, 6286, 6287, 6288];
+	return lengths.includes(cardNumber.length) && (containsPrefix(prefixes, prefix) || withinRange(getPrefix(cardNumber, 6), 622126, 622925));
 };
 
 const checkSwitch = function(cardNumber) {
-	const sLength = [16, 18, 19];
-	const sPrefix = [4903, 4905, 4911, 4936, 6333, 6759, 564182, 633110];
-	return sLength.includes(cardNumber.length) && sPrefix.includes(getPrefix(cardNumber, 4)) || sPrefix.includes(getPrefix(cardNumber, 6));
+	let prefix = [getPrefix(cardNumber, 4), getPrefix(cardNumber, 6)];
+	const lengths = [16, 18, 19];
+	const prefixes = [4903, 4905, 4911, 4936, 6333, 6759, 564182, 633110];
+	return lengths.includes(cardNumber.length) && containsPrefix(prefixes, prefix);
 };
 
 var detectNetwork = function(cardNumber) {
